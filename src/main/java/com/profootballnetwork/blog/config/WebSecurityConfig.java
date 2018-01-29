d82a999b6df83effa179bcce1d7070ebcb5db5ca
marwensaidi <marwensaidi1@gmail.com>
2018-01-29 20:48:20 +0100

    clean repo

--- src/main/java/com/profootballnetwork/blog/config/WebSecurityConfig.java
+++ src/main/java/com/profootballnetwork/blog/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.profootballnetwork.blog.config;
 
 import com.profootballnetwork.blog.security.*;
-import com.sivalabs.blog.security.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
